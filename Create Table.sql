ca0f460719a73dfb5b50931a6f2f48f0b843e81a
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2023-05-09 23:10:45 +0600

    Create Create Table.sql

---
+++ Create Table.sql
@@ -0,0 +1,6 @@
+-- 1. Write a SQL statement to create a simple table countries including columns country_id,country_name and region_id.
+
+create table project01.Countries (
+country_id int,
+country_name varchar(20),
+region_id int);
