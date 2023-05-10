3beb03f056604f449a34f8c6a59345e0f5015a78
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2023-05-10 22:09:34 +0600

    Update Create Table.sql

--- Create Table.sql
+++ Create Table.sql
@@ -4,3 +4,19 @@ create table project01.Countries (
 country_id int,
 country_name varchar(20),
 region_id int);
+
+
+-- Write a SQL statement to create a simple table countries including columns country_id,country_name and region_id which is already exists.
+
+Create Table IF NOT EXISTS countries(
+country_id int, 
+country_name varchar(20),
+region_id int); 
+
+/* 
+01. "EXISTS"
+IF NOT EXISTS- Something new to me! 
+01. The result of EXISTS is a boolean value True or False. 
+02. It can be used in a SELECT, UPDATE, INSERT or DELETE statement.
+03. Checks whether correlated NESTED QUERIES are empty or Not.
+*/
