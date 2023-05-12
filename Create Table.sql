33d251052163fdc5fbb4e342a196d8f3fa769c6c
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2023-05-12 12:20:50 +0600

    Update Create Table.sql

--- Create Table.sql
+++ Create Table.sql
@@ -8,7 +8,7 @@ region_id int);
 
 -- 2. Write a SQL statement to create a simple table countries including columns country_id,country_name and region_id which is already exists.
 
-Create Table IF NOT EXISTS countries(
+Create Table IF NOT EXISTS project01.countries(
 country_id int, 
 country_name varchar(20),
 region_id int); 
@@ -23,5 +23,12 @@ IF NOT EXISTS- Something new to me!
 
 
 -- 3. Write a SQL statement to create the structure of a table dup_countries similar to countries.
-Create Table IF NOT EXISTS dup_countries 
-LIKE countries; 
+Create Table IF NOT EXISTS project01.dup_countries 
+LIKE project01.countries; 
+
+-- ***For each table name, database should be mentioned. 
+
+-- DESC 
+DESC project01.dup_countries; 
+
+-- By this query, we can get all the details of a created table. 
