ffece4882a5412fd69ed1773af864e9d08011902
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2023-05-24 12:28:54 +0600

    Update Create Table.sql

--- Create Table.sql
+++ Create Table.sql
@@ -192,7 +192,18 @@ DESC project01.countries5;
 14. Write a SQL statement to create a table job_history including columns employee_id, start_date, end_date, job_id and department_id 
 and make sure that, the employee_id column does not contain any duplicate value at the time 
 of insertion and the foreign key column job_id contain only those values which are exists in the jobs table.
+
++------------+--------------+------+-----+---------+-------+
+| Field      | Type         | Null | Key | Default | Extra |
++------------+--------------+------+-----+---------+-------+
+| JOB_ID     | varchar(10)  | NO   | PRI |         |       |
+| JOB_TITLE  | varchar(35)  | NO   |     | NULL    |       |
+| MIN_SALARY | decimal(6,0) | YES  |     | NULL    |       |
+| MAX_SALARY | decimal(6,0) | YES  |     | NULL    |       |
++------------+--------------+------+-----+---------+-------+
 */
+
+
 Create Table project01.jobs2
 (job_id int NOT NULL PRIMARY KEY,
 job_title varchar(20),
@@ -217,3 +228,57 @@ FOREIGN KEY (job_id) REFERENCES project01.jobs2(job_id));
 DESC project01.job_history; 
 
 
+
+
+
+/*
+15. Write a SQL statement to create a table employees including columns employee_id, first_name, last_name, email, phone_number hire_date, job_id, salary, 
+commission, manager_id and department_id and make sure that, the employee_id column does not contain any duplicate value at the time of insertion and the 
+foreign key columns combined by department_id and manager_id columns contain only those unique combination values, which combinations are exists in the 
+departments table.
+
+Assume the structure of departments table below.
+
++-----------------+--------------+------+-----+---------+-------+
+| Field           | Type         | Null | Key | Default | Extra |
++-----------------+--------------+------+-----+---------+-------+
+| DEPARTMENT_ID   | decimal(4,0) | NO   | PRI | 0       |       |
+| DEPARTMENT_NAME | varchar(30)  | NO   |     | NULL    |       |
+| MANAGER_ID      | decimal(6,0) | NO   | PRI | 0       |       |
+| LOCATION_ID     | decimal(4,0) | YES  |     | NULL    |       |
++-----------------+--------------+------+-----+---------+-------+
+
+*/
+
+
+Create Table exercises.departments
+(
+department_id decimal(4,0) DEFAULT 0 primary key,
+department_name varchar(30) NOT NULL,
+manager_id decimal(6,0) DEFAULT 0,
+location_id decimal(4,0)
+); 
+
+
+
+Create Table exercises.employees
+(
+employee_id decimal(6,0) NOT NULL PRIMARY KEY,
+first_name varchar(50) NOT NULL,
+last_name varchar(50) NOT NULL, 
+email varchar(100) NOT NULL, 
+phone_number decimal(11,0) NOT NULL, 
+hire_date date NOT NULL, 
+job_id decimal(4,0) NOT NULL, 
+salary decimal(10,0) NOT NULL, 
+commission decimal(3,0) NOT NULL, 
+manager_id decimal(6,0) DEFAULT NULL,
+department_id decimal(4,0) DEFAULT NULL,
+FOREIGN KEY(department_id,manager_id) REFERENCES exercises.departments(department_id,manager_id)
+);
+
+
+DESC exercises.employees; 
+
+
+-- Error
